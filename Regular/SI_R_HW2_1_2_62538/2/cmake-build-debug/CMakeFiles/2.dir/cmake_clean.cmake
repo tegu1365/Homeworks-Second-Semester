bf2d16a9a15be7183181a55c8dc9c96baeda6e99
tegu1365 <tedi.d.petkova@gmail.com>
2021-04-17 16:24:09 +0300

    HW 2 Regular Problem 2: Started

---
+++ Regular/SI_R_HW2_1_2_62538/2/cmake-build-debug/CMakeFiles/2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2.exe"
+  "2.exe.manifest"
+  "2.lib"
+  "2.pdb"
+  "CMakeFiles/2.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
