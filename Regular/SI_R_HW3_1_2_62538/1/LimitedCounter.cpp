1d75e1b1f0a2b3f51869beff6e36f259cf120d08
tegu1365 <tedi.d.petkova@gmail.com>
2021-05-04 14:08:47 +0300

    HW 3 Regular problem 1: done with LimitedCounter-class

--- Regular/SI_R_HW3_1_2_62538/1/LimitedCounter.cpp
+++ Regular/SI_R_HW3_1_2_62538/1/LimitedCounter.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "LimitedCounter.hpp"
+
+LimitedCounter::LimitedCounter(const int max, const int initial, const unsigned int step):Counter(initial, step) {
+    this->max=max;
+}
+
+void LimitedCounter::increment() {
+    if(total<max) {
+        Counter::increment();
+    }
+}
+
+int LimitedCounter::getMax() const {
+    return max;
+}
--- Regular/SI_R_HW3_1_2_62538/1/LimitedCounter.hpp
+++ Regular/SI_R_HW3_1_2_62538/1/LimitedCounter.hpp
@@ -4,10 +4,15 @@
 
 #ifndef INC_1_LIMITEDCOUNTER_HPP
 #define INC_1_LIMITEDCOUNTER_HPP
+#include "Counter.hpp"
 
-
-class LimitedCounter {
-
+class LimitedCounter:virtual public Counter{
+private:
+    int max;
+public:
+    LimitedCounter(const int max,const int initial=0,const unsigned int step=1);
+    void increment();
+    int getMax()const;
 };
 
 
